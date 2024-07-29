b6934817d119b615cf499486cb8266b38e1ffbd8
Tomasz Brylski <brylskitomasz@gmail.com>
2024-07-29 23:21:12 +0200

    Created a new let declaration for button that dynamically rotates the <a> element depending on the mouse cursor position.

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ let weightElement = document.querySelector(".js-weight");
 let bmiElement = document.querySelector(".js-bmi");
 
 formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    let height = heightElement.value;
-    let weight = weightElement.value;
+  let height = heightElement.value;
+  let weight = weightElement.value;
 
-    let bmi = weight /  ((height / 100) ** 2);
+  let bmi = weight / (height / 100) ** 2;
 
-    bmiElement.innerText = bmi.toFixed(2);
+  bmiElement.innerText = bmi.toFixed(2);
+});
 
+let btn = document.querySelector("a");
+btn.addEventListener("mousemove", (e) => {
+  let rect = e.target.getBoundingClientRect();
+  let x = e.clientX * 3 - rect.left;
+  btn.style.setProperty("--x", x + "deg");
 });
