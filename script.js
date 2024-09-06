fd7e6fd873f4976317a00425d0807aa56ded0c0a
Tomasz Brylski <brylskitomasz@gmail.com>
2024-09-06 11:42:28 +0200

    Placed functions in a block and changed from ''let'' functions to ''const''.

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let heightElement = document.querySelector(".js-height");
-let formElement = document.querySelector(".js-form");
-let weightElement = document.querySelector(".js-weight");
-let bmiElement = document.querySelector(".js-bmi");
+{
+const heightElement = document.querySelector(".js-height");
+const formElement = document.querySelector(".js-form");
+const weightElement = document.querySelector(".js-weight");
+const bmiElement = document.querySelector(".js-bmi");
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let height = heightElement.value;
-  let weight = weightElement.value;
 
-  let bmi = weight / (height / 100) ** 2;
+  const height = heightElement.value;
+  const weight = weightElement.value;
+
+  const bmi = weight / (height / 100) ** 2;
 
   bmiElement.innerText = bmi.toFixed(2);
 });
 
-let btn = document.querySelector("a");
+const btn = document.querySelector("a");
 btn.addEventListener("mousemove", (e) => {
-  let rect = e.target.getBoundingClientRect();
-  let x = e.clientX * 3 - rect.left;
+  const rect = e.target.getBoundingClientRect();
+  const x = e.clientX * 3 - rect.left;
   btn.style.setProperty("--x", x + "deg");
 });
+}
