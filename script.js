44b5b2821de3819e3f9137950a2e71a8d75e555f
Tomasz Brylski <brylskitomasz@gmail.com>
2024-09-09 11:17:58 +0200

    - Formatted code, - Functions have been added to the appropriate elements,   which makes it easier to read and the code is clear.

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 {
-const heightElement = document.querySelector(".js-height");
-const formElement = document.querySelector(".js-form");
-const weightElement = document.querySelector(".js-weight");
-const bmiElement = document.querySelector(".js-bmi");
+  const resultBmi = (event) => {
+    const weightElement = document.querySelector(".js-weight");
+    const heightElement = document.querySelector(".js-height");
+    const bmiElement = document.querySelector(".js-bmi");
+    event.preventDefault();
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+    const height = heightElement.value;
+    const weight = weightElement.value;
 
+    const bmi = weight / (height / 100) ** 2;
 
-  const height = heightElement.value;
-  const weight = weightElement.value;
+    bmiElement.innerText = bmi.toFixed(2);
+  };
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", resultBmi);
+  };
 
-  const bmi = weight / (height / 100) ** 2;
+  const btn = document.querySelector("a");
+  btn.addEventListener("mousemove", (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX * 3 - rect.left;
+    btn.style.setProperty("--x", x + "deg");
+  });
 
-  bmiElement.innerText = bmi.toFixed(2);
-});
-
-const btn = document.querySelector("a");
-btn.addEventListener("mousemove", (e) => {
-  const rect = e.target.getBoundingClientRect();
-  const x = e.clientX * 3 - rect.left;
-  btn.style.setProperty("--x", x + "deg");
-});
+  init();
 }
